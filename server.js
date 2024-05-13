39b95d337556b942563dd1948bdb7139408b72fb
Reshma Shaik <reshma786jrrs@gmail.com>
2024-05-14 00:43:09 +0530

    second commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 
+const cors = require('cors')
 const mongoose = require('mongoose')
 
 const workoutRoutes = require('./routes/workouts')
@@ -12,11 +13,19 @@ const UserRoutes = require('./routes/user')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('public'))
+app.use(express.static('files'))
+
+app.use(cors({
+    origin: ' https://workoutapi-v3lm.onrender.com', // Allow requests from this origin
+    credentials: true, // Allow credentials (e.g. cookies) to be sent with the request
+  }));
 
 app.use((req, res, next) =>{
     console.log(req.path, req.method)
     next()
 })
+
 
 
 app.use('/api/workouts' ,workoutRoutes)
