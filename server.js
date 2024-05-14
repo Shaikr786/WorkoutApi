b903b9c8180842a0fdb1d86866910e13d7596ef9
Reshma Shaik <reshma786jrrs@gmail.com>
2024-05-14 10:39:01 +0530

    fourth commit

--- server.js
+++ server.js
@@ -26,8 +26,11 @@ app.use(cors({
     maxAge: 86400
   }));
   
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+ 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   });
 
