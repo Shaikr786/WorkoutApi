98c48f606ef072b6d65a40269ed0ea902c78e0f3
Reshma Shaik <reshma786jrrs@gmail.com>
2024-05-14 10:20:49 +0530

    third commit

--- server.js
+++ server.js
@@ -15,15 +15,19 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: ' https://workoutapi-v3lm.onrender.com', // Allow requests from this origin
-    credentials: true, // Allow credentials (e.g. cookies) to be sent with the request
+    origin: 'http://localhost:3000',
+    credentials: true,
+    exposedHeaders: ['Content-Disposition'],
+    headers: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    maxAge: 86400
   }));
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://workoutapi-v3lm.onrender.com');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    console.log(req.path, req.method)
+  
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', true);
     next();
   });
 
